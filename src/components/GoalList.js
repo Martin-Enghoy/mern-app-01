2b550ec403f76a39b62a7f67b685412fcc08fb6b
Martin-Enghoy <adrianenghoy@gmail.com>
2024-12-19 16:37:56 +0800

    feat: add props passed in GoalsList component

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const GoalList = () => {
+const GoalList = ({goals}) => {
+    console.log(goals)
     return (
         <ul className="goal-list">
-            <li>Finish the Course</li>
-            <li>Learn all about the Course Main Topic</li>
-            <li>Help other students in the Course Q&amp;A</li>
+            {goals.map(goal => (
+                <li key={goal.id}>{goal.text}</li>
+            ))}
         </ul>
     );
 };
+
+GoalList.propTypes = {
+    goals: PropTypes.object,
+}
 
 export default GoalList;
