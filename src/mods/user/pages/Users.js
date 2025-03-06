c197b895760a76f6bfd0b3a6be31fac3ca8492d7
Martin-Enghoy <adrianenghoy@gmail.com>
2025-03-06 15:01:02 +0800

    feat: add UsersList and UserItem in Users page

--- src/mods/user/pages/Users.js
+++ src/mods/user/pages/Users.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import UsersList from "../../../components/UsersList/UsersList";
+
+const USERS = [
+  { id: 'u1', name: 'Martin Enghoy', image: 'https://i.imgur.com/jtWM1xS.png', places: 3 }
+]
+
 
 const Users = () => {
-  return <h2>User page here!</h2>
+
+  return <UsersList items={USERS} />
 }
 
 export default Users;
